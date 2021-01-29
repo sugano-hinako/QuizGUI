686f565cf54b2c9bbc121580a4271978b9d7c5ee
hinako <hinako@192.168.3.2>
2021-01-29 17:25:49 +0900

    定数クラス

---
+++ src/main0115/Const.java
@@ -0,0 +1,24 @@
+package main0115;
+
+public class Const {
+	//定数
+	public static final String GOTHIC = "ＭＳ ゴシック";
+	public static final String BUTTON = "button";
+	public static final String BUTTON2 = "button2";
+	public static final String BUTTON3 = "button3";
+	public static final String BUTTON4 = "button4";
+	public static final String BUTTON5 = "button5";
+	public static final String BUTTON6 = "button6";
+	public static final String NEXT = "next";
+	public static final String NEXT2 = "next2";
+	public static final String NEXT3 = "next3";
+	public static final String NEXT4 = "next4";
+	public static final String NEXT5 = "next5";
+	public static final String NEXT6 = "next6";
+	public static final String NEXT7 = "next7";
+	public static final String NEXT8 = "next8";
+	public static final String NEXT9 = "next9";
+	public static final String NEXT10 = "next10";
+	public static final String CORRECT = "正解!!";
+
+}
